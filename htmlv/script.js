3fd253457479d10dcc9411883ee7f70300451428
Kirill Kataman <kirkat0512@gmail.com>
2024-01-16 17:15:25 +0100

    added scroll

--- htmlv/script.js
+++ htmlv/script.js
@@ -1,14 +1,24 @@
-// const header = document.querySelector('header');
-// const nextSection = document.querySelector('#next-section');
+let header_btn = document.getElementsByClassName("header_btn");
 
-// const observer = new IntersectionObserver((entries) => {
-//     entries.forEach((entry) => {
-//         if (entry.isIntersecting) {
-//             header.classList.add('active');
-//         } else {
-//             header.classList.remove('active');
-//         }
-//     });
-// });
 
-// observer.observe(nextSection);
+    
+
+function scrollToSection(sectionId) {
+    let section = document.getElementById(sectionId);
+    if (section != null && section != "section1") {
+      let offset = section.offsetTop - 80;
+  
+      document.documentElement.scrollTop = offset;
+      document.body.scrollTop = offset;
+
+    }   if(sectionId === "section1"){
+      document.documentElement.scrollTop = 0;
+      document.body.scrollTop = 0;
+    }
+
+    for(let i = 0; i < header_btn.length; i++){
+      header_btn[i].classList.remove("active");
+    }
+    document.getElementById(sectionId).classList.add("active");
+
+  }
